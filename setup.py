51950b77cdfabc8aeb885a7221907c9502bbe548
KY00KIM <>
2024-09-08 18:22:10 +0800

    Add CUDA impl

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
 
 setup(name='lltm_cpp',
       ext_modules=[cpp_extension.CppExtension('lltm_cpp', ['lltm.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
+
+setup(
+    name='lltm',
+    ext_modules=[
+        CUDAExtension('lltm_cuda', [
+            'lltm_cuda.cpp',
+            'lltm_cuda_kernel.cu',
+        ])
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
